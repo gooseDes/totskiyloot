95dc9accf4d79078fb5fd361fb31945db96b5860
Goose Des <166229741+gooseDes@users.noreply.github.com>
2025-03-18 23:27:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    document.getElementById("userButton").addEventListener("click", function() {
+        window.location.href = "signin.html";
+    });
+
+    document.getElementById("navButton").addEventListener("click", function() {
+        if (window.matchMedia("(orientation: landscape)").matches) {
+            document.getElementById("sideNav").style.width = "300px";
+        } else {
+            document.getElementById("sideNav").style.width = "100%";
+        }
+        document.getElementById("content").style.filter = "blur(10px)";
+    });
+
+    document.getElementById("closeNav").addEventListener("click", function() {
+        document.getElementById("sideNav").style.width = "0";
+        document.getElementById("content").style.filter = "";
+    });
+
+    setInterval(function() {
+        var ars = document.querySelectorAll(".ar9_16");
+        ars.forEach((ar) => {
+            if (window.innerWidth/window.innerHeight > 9/16) {
+                ar.style.aspectRatio = "9/16";
+            } else {
+                ar.style.aspectRatio = "";
+            }
+        });
+    }, 1/30);
+};
