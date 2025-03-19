3fb2dadaab1d0b7ebc4afe7b05bee4cd2abcaffe
gooseDes <sasaprst32@gmail.com>
2025-03-19 15:45:39 +0200

    a

---
+++ signin.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function() {
+    document.getElementById("signin_button").addEventListener("click", function() {
+        window.location.href = "default.html"
+    });
+});
