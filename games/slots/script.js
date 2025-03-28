53627caf5de83c2c2c9020f3683e4a078918b5a4
Goose Des <sasaprst32@gmail.com>
2025-03-28 21:19:12 +0200

    totskiy

--- games/slots/script.js
+++ games/slots/script.js
@@ -1,34 +1,28 @@
 var top = 0;
 
 window.addEventListener("load", function() {
-    document.querySelectorAll('.slot').forEach((slot) => {
-        top = parseInt(slot.style.top.slice(0, -3))
-    });
     document.getElementById('exitButton').addEventListener("click", function() {
         goto('/games');
     });
+    
     document.getElementById('spinButton').addEventListener("click", function() {
         document.getElementById('spinButton').classList.add('spinning');
         document.querySelectorAll('.slot').forEach((slot) => {
-            for (let i = 0; i < 15; i++) {
+            for (let i = 0; i < 20; i++) {
                 setTimeout(function() {
-                    slot.style.height = '0vw';
-                    slot.style.transitionDuration = ((300-i*20)/1000).toString().concat('s');
+                    slot.style.height = '0%';
+                    slot.style.width = '90%';
+                    slot.style.transitionDuration = ((i*21)/1000).toString().concat('s');
                     setTimeout(function() {
                         slot.innerHTML = Math.round(Math.random()*9);
-                        slot.style.height = '36vw';
-                    }, 300-i*20);
-                }, i*(600-i*20));
+                        slot.style.height = '100%';
+                        slot.style.width = '100%';
+                    }, i*21);
+                }, i*(i*21));
             }
         });
         setTimeout(function() {
             document.getElementById('spinButton').classList.remove('spinning');
         }, 2000)
     });
-    setInterval(function() {
-        document.querySelectorAll('.slot').forEach((slot) => {
-            top = 32 + (42 - slot.style.offsetHeight)/2;
-            slot.style.top = top.toString().concat('vw');
-        });
-    }, 1/30);
 });
