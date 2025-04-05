c54062a616cfbb41a07ee14f8198455616d1e144
Goose Des <sasaprst32@gmail.com>
2025-04-05 18:05:06 +0300

    totskiy

--- games/test/script.js
+++ games/test/script.js
@@ -1,8 +1,8 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.175.0/build/three.module.min.js';
+const THREE = await import('https://esm.sh/three@0.175.0');
+const { GLTFLoader } = await import('https://esm.sh/three@0.175.0/examples/jsm/loaders/GLTFLoader.js');
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x222244);
-
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
@@ -11,23 +11,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+const loader = new GLTFLoader();
+var car = null;
+
+loader.load('models/car/scene.gltf', (gltf) => {
+  car = gltf.scene;
+  scene.add(gltf.scene);
+});
+
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+//scene.add(cube);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 1);
+const pointLight = new THREE.PointLight(0xffffff, 2);
 pointLight.position.set(5, 5, 5);
 scene.add(pointLight);
 
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  car.rotation.x += 0.01;
+  car.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
